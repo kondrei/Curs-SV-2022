bd462117e4e7be524c3d6bb2f81215235771b511
kondrei <kondrei@gmail.com>
2022-05-04 15:42:12 +0300

    change gallery settings

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //close image on click
 let background = document.getElementById('background')
 background.addEventListener('click', () =>
-    background.style.visibility = 'hidden')
+    background.style.display = 'none')
 
 //get images
 let images = document.getElementById('galerie').childNodes
@@ -9,8 +9,8 @@ let images = document.getElementById('galerie').childNodes
 for (i = 0; i < images.length; i++) {
     if (images[i].tagName) {
         images[i].children[0].addEventListener('click', (e) => {
+            background.style.display = 'flex'
             background.style.position = 'fixed'
-            background.style.visibility = 'visible'
             document.getElementById('currentImage').src = e.target.src
             document.getElementById('caption').innerText = e.target.alt
         })
