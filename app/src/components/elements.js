73a164fb92b1a60e9c6827d07f5c17f155216ee5
kondrei <kondrei@gmail.com>
2022-05-16 08:50:05 +0300

    Adding gradients to squares

--- app/src/components/elements.js
+++ app/src/components/elements.js
@@ -1,47 +1,55 @@
-import { useEffect, useState } from 'react'
-import Square from './square';
-import { v4 as uuidv4 } from 'uuid';
+import { useEffect, useState } from "react";
+import Square from "./square";
+import { v4 as uuidv4 } from "uuid";
 
 const Elements = () => {
-
+    const randomColor = () =>
+        "#" + (Math.random().toString(16) + "000000").substring(2, 8);
     const [elements, setElements] = useState([]);
-    const [color, setColor] = useState('#' + (Math.random().toString(16) + "000000").substring(2, 8));
-    const [gradient, setGradient] = useState([]);
+    const [color, setColor] = useState(randomColor());
 
     const changeColor = (e) => {
         setColor(e.target.value);
-    }
+    };
 
     const addElements = () => {
-        const neweElement = [...elements]
-        neweElement.push({
+        const newElement = [...elements];
+        newElement.push({
             id: uuidv4(),
-            color: color
+            color:
+                elements.length === 0
+                    ? color
+                    : `${elements[elements.length - 1].color} , ${color}`
         });
-        setElements(neweElement);
-    }
+
+        setElements(newElement);
+    };
 
     useEffect(() => {
-        setGradient(elements.map(element => element.color));
-        setColor('#' + (Math.random().toString(16) + "000000").substring(2, 8));
+        setColor(randomColor());
     }, [elements]);
 
     const remove = (id) => {
         const remainingElements = elements.filter((element) => element.id !== id);
         setElements(remainingElements);
-    }
+    };
 
     return (
-        <div className='card elements'>
+        <div className="card elements">
             <div className="controls">
                 <input type="color" value={color} onChange={changeColor} />
                 <input type="button" value="Add" onClick={addElements} />
             </div>
             {elements.map((element) => (
-                <Square key={element.id} id={element.id} color={gradient} remove={remove} />
+                <Square
+                    key={element.id}
+                    id={element.id}
+                    color={element.color}
+                    remove={remove}
+                />
             ))}
         </div>
     );
-}
+};
 
 export default Elements;
